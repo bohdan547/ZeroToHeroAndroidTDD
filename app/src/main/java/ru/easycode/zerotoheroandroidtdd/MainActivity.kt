49342fb59d6e014bbf85e3c25975745009ca7a25
bohdan547 <129954499+bohdan547@users.noreply.github.com>
2024-07-02 01:46:45 +0300

    task 008 done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,33 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var Text: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Text = findViewById(R.id.titleTextView)
+        val Buton = findViewById<Button>(R.id.hideButton)
+
+        Buton.setOnClickListener {
+            Text.visibility = View.INVISIBLE
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("key", Text.visibility)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Text.visibility = savedInstanceState.getInt("key")
+    }
+
 }
