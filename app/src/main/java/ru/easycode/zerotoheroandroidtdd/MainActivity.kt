dcd6aa35bbf5b0704420fb88a74f3190cf7bc2b8
bohdan547 <129954499+bohdan547@users.noreply.github.com>
2024-07-17 15:54:58 +0300

    task 011 done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,36 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private val count = Count.Base(2)
+    private lateinit var Text: TextView
+    private lateinit var Button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Text = findViewById(R.id.countTextView)
+        Button = findViewById(R.id.incrementButton)
+
+        Button.setOnClickListener {
+            val result = count.increment(Text.text.toString())
+            Text.text = result
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("key", Text.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Text.text = savedInstanceState.getString("key")
+    }
+
 }
+
